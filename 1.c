b83229a5b0d92e26e1fb5be353249cb04ee755fb
Kylemerian <67842019+Kylemerian@users.noreply.github.com>
2020-11-01 15:05:39 +0300

    stable_4_step

--- 1.c
+++ 1.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 typedef struct list {
     char * str;
@@ -55,15 +56,55 @@ void freemem(list * headlist)
     }
 }
 
-
 list * addtolist(list * head, char * str, int lenbuff)
 {
-    list * tmp = malloc(sizeof( * tmp));
+    list * tmp = malloc(sizeof(* tmp));
     tmp -> str = malloc(lenbuff + 1);
     strncpy(tmp -> str, str, lenbuff);
     tmp -> str[lenbuff] = 0;
     tmp -> next = head;
     return tmp;
+}
+
+char * getFilename(int key, list * headlist)
+{
+    int it = 1;
+    list * tpointer = headlist;
+    while(tpointer != NULL){
+        if(key == it)
+            return tpointer->str;
+        it++;
+    }
+    /*printf("%s ", tpointer->str);*/
+    return tpointer->str;
+}
+
+int changeIO(list * head, int * inOut)
+{
+    int fd;
+    int errflag = 0;
+    if(inOut[0]){
+        fd = open(getFilename(inOut[0], head), O_RDONLY | O_CREAT);
+        if(fd != -1)
+            dup2(fd, 0);
+        else
+            errflag = 1;
+    }
+    if(inOut[2]){
+        fd = open(getFilename(inOut[2], head),O_WRONLY | O_APPEND | O_CREAT, 0666);
+        if(fd != -1)
+            dup2(fd, 1);
+        else
+            errflag = 1;
+    }
+    if(inOut[2] == 0 && inOut[1]){
+        fd = open(getFilename(inOut[1], head), O_WRONLY | O_CREAT, 0666);
+        if(fd != -1)
+            dup2(fd, 1);
+        else
+            errflag = 1;
+    }
+    return errflag;
 }
 
 int listsize(list * headlist)
@@ -77,27 +118,83 @@ int listsize(list * headlist)
     return (iterator - 1);
 }
 
-int spaceortab(int c)
+void handleIOquote(int lastc, int c, int * inOut, list * headlist, int i)
 {
-    return ((c == ' ') || (c == '\t'));
+    int size = listsize(headlist) + 1;
+    if(i > 0)
+        size++;
+    if(c == '<')
+        if(inOut[0] == 0)
+            inOut[0] = size;
+        else
+            inOut[0] = -1;
+    else if(c == '>' && lastc == '>')
+        if(inOut[2] == 0)
+            inOut[2] = size;
+        else
+            inOut[0] = -1;
+    else if(c == '>')
+        if(inOut[1] == 0)
+            inOut[1] = size;
+        else
+            inOut[0] = -1;
+    else
+        inOut[0] = -1;
+}
+
+int isIOsymbol(int c)
+{
+    return ((c == '>') || (c == '<'));
+}
+
+int spaceTab(int c)
+{
+    return ((c == '\t') || (c == ' '));
+}
+
+int spaceTabIOquote(int c)
+{
+    return ((c == ' ') || (c == '\t') || (c == '<') || (c == '>'));
 }
 
 int separator(int c)
 {
-    return ((c == ' ') || (c == '\t') || (c == '&'));
+    return ((c == ' ') || (c == '\t') || (c == '&') || (c == '>') || (c == '<'));
 }
 
-char ** makearr(list * headlist, int size)
+int inIO(int key, int * inOut)
+{
+    int i;
+    int inFlag = 0;
+    for(i = 0; i < 3; i++)
+        if(inOut[i] == key){
+            inFlag = 1;
+            break;
+        }
+    return inFlag;
+}
+
+char ** makearr(list * headlist, int size, int * inOut)
 {
     list * tmp = headlist;
-    int i;
+    int i, j;
+    int sizeIO = 0;
+    for (int i =0; i < 3; i++)
+        if(inOut[i] != 0)
+            sizeIO++;
+    if(inOut[2] > 0)
+        sizeIO--;
+    j = size - 1 - sizeIO;
     char ** arr = malloc((size + 1) * sizeof(*arr));
     for(i = size - 1; i >= 0; i--, tmp = tmp->next){
-        arr[i] = malloc(strlen(tmp->str) + 1);
-        strncpy(arr[i], tmp->str, strlen(tmp->str));
-        arr[i][strlen(tmp->str)] = 0;
+        if(!inIO(i + 1, inOut)){
+            arr[i] = malloc(strlen(tmp->str) + 1);
+            strncpy(arr[i], tmp->str, strlen(tmp->str));
+            arr[i][strlen(tmp->str)] = 0;
+            j--;
+        }
     }
-    arr[size] = (char*)NULL;
+    arr[size - sizeIO] = (char*)NULL;
     return arr;
 }
 
@@ -116,26 +213,25 @@ void changedir(char ** arr, int size)
         printf("Too many args\n");
 }
 
-void execute(list * headlist, int mode)
+void execute(list * headlist, int mode, int * inOut)
 {
-    int j;
-    pid_t pid;
+    int j, pid;
     int size = listsize(headlist);
-    char ** arr = makearr(headlist, size);
-    if(!strcmp(arr[0], "cd")){
+    char ** arr = makearr(headlist, size, inOut);
+    if(!strcmp(arr[0], "cd"))
         changedir(arr, size);
-    }
     else{
         pid = fork();
         if(pid == 0){
-            execvp(arr[0], arr);
+            if(!changeIO(headlist, inOut)){
+                execvp(arr[0], arr);
+            }
             perror(NULL);
             exit(1);
         }
-        if(mode == 0){
+        if(mode == 0)
             while(wait(NULL) != pid)
                 ;
-        }
     }
     for(j = 0; j < size; j++)
           free(arr[j]);
@@ -155,31 +251,49 @@ void processinglast(int * iterator, char * buff, list **headlist)
     *iterator = 0;
 }
 
-void iscorrectcommand(int * quoteflag, int * ampers, int chr, list ** headlist)
+int isWrongIO(list * headlist, int * inOut)
 {
-    int mode = (*ampers == 1);
-    if (*quoteflag || (*ampers > 1) || (*ampers == 1 && chr != '&'))
+    int size = listsize(headlist);
+    int errflag = 0;
+    int i = 0;
+    for(i = 0; i < 3; i++)
+        if(inOut[i] > size)
+            errflag = 1;
+    if(inOut[0] == -1)
+        errflag = 1;
+    return errflag;
+}
+
+void iscorrectcmd(int * qflag, int * amper, int chr, list ** head, int * inOut)
+{
+    int i;
+    int mode = (*amper == 1);
+    if (*qflag || (*amper > 1) || (*amper == 1 && chr != '&')
+        || isWrongIO(*head, inOut))
+
         printf("incorrect input\n");
-    else if((*headlist)->str != NULL)
-        execute(*headlist, mode);
-    *quoteflag = 0;
-    *ampers = 0;
+    else if((*head)->str != NULL){
+        execute(*head, mode, inOut);
+        /*printlist(*head);*/
+    }
+    *qflag = 0;
+    *amper = 0;
+    for(i = 0; i < 3; i++)
+        inOut[i] = 0;
 }
 
 void killbg()
 {
-    while(wait(NULL) != -1)
+    while(wait4(-1, NULL, WNOHANG, NULL) > 0)
         ;
 }
 
 int main()
 {
-    int c;
-    int lastchar = 0;
-    int ampers = 0;
-    int lenbuff = 8;
-    int i = 0;
-    int quoteflag = 0;
+    int c, lastchar = 0;
+    int ampers = 0, lenbuff = 8;
+    int i = 0, quoteflag = 0;
+    int inOutPos[3] = {0, 0, 0};
     char * buff = malloc(lenbuff);
     list * headlist = NULL;
     headlist = init(headlist);
@@ -191,26 +305,28 @@ int main()
                 buff[i] = c;
                 i++;
             }
+            else if (isIOsymbol(c))
+                handleIOquote(lastchar, c, inOutPos, headlist, i);
             else if (c == '\"')
                 quoteflag = !quoteflag;
             else if (c == '&')
                 ampers++;
-            else if (i != 0) {
+            if (i != 0 && !quoteflag && spaceTabIOquote(c)) {
                 headlist = addtolist(headlist, buff, i);
                 i = 0;
             }
-            if(!spaceortab(c))
+            if(!spaceTab(c))
                 lastchar = c;
         }
         else{
             processinglast(&i, buff, &headlist);
-            iscorrectcommand(&quoteflag, &ampers, lastchar, &headlist);
+            iscorrectcmd(&quoteflag, &ampers, lastchar, &headlist, inOutPos);
             headlist = reinit(&headlist);
+            killbg();
         }
     }
     freemem(headlist);
     free(buff);
-    killbg();
     printf("\n");
     return 0;
 }
